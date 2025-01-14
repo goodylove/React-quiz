e9ab8cf7f4c302be61b0f03e0276d895ad21ab8a
goodylove <goodyc474@gmail.com>
2025-01-14 07:23:28 +0100

    add reducer function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+  error: null,
+  state: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+  }
+}
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function (props) {
+    fetch("http://localhost:8000/questions")
+      .then((response) => response.json().then((res) => console.log(res)))
+      .catch((err) => console.log(err.message));
+  }, []);
+
   return (
     <>
       <Header />
